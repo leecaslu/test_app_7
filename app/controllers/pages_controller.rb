74d854e8cfbc5d45746d013033a04804fe122900
leecaslu <lucashollerlee@gmail.com>
2023-02-20 15:05:31 -0300

    comentando

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    # essa funcao fica vazia pois a classe vai procurar nos views o metodo para renderizar. O padrão é renderizar o que estiver no view.
   end
 end
