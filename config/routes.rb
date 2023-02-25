ccf046347a627960298be3e21e1a2bce0cb5ea1f
leecaslu <lucashollerlee@gmail.com>
2023-02-25 12:08:39 -0300

    using the scaffold command

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
   # criando um route para o root. Usamos a sintaxe 'nome do controller#método para especificar qual vai ser o método utilizado/renderizado.'
   root 'pages#home'
   get 'about', to: 'pages#about'
